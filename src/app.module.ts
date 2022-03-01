e127295ae567d06953e28936510f1dbee3cd2064
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-03-01 17:08:18 +0100

    include static folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path/posix';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [ChatGateway],
 })
