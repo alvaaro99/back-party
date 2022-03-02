17ff2ee96daf1fc5b016aa58dcfee55415c7fc03
Alvaro Acosta <alvaro99acosta@gmail.com>
2022-03-02 18:13:12 +0100

    get my profile

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/models/user';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async myUser(@Req() req: Request, @Res() res: Response) {
+    const { password, ...user } = this.userService.findByEmail(
+      (req.user as User).email,
+    );
+    res.status(HttpStatus.OK).json(user);
+  }
+}
